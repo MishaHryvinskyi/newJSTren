2e5d30ebfb45641c359674c3af5851c750acb3b1
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-18 11:59:10 +0300

    modul 1-1

---
+++ js/tasr1.js
@@ -0,0 +1,31 @@
+let color = document.body.style.backgroundColor = "skyblue";
+let number = document.body.textContent = 'Качеча чебуреча';
+
+
+// const shouldRenew = confirm("Йо чи не йо? Якщо йо! тицяй ок! а якщо не йо то тикай кансл!");// повертає true або false
+// console.log(shouldRenew);
+// console.log("shouldRenew:", typeof(shouldRenew)); 
+
+// let quantity = prompt('введіть кількість бринзи');
+// quantity = String(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = "50px"; 
+// const resultWidth = Number.parseInt(elementWidth); // запинається на першому символі з ліва на право і на першому не числі закінчує парсити
+// console.log(elementWidth);
+// console.log(resultWidth);
+ 
+// let elementHeight = "200.50px";
+// const resultHeight = Number.parseFloat(elementHeight);
+// console.log(typeof elementHeight);
+// console.log(typeof resultHeight);
+
+// let salary = 1300.16472;
+// // salary = salary.toFixed(3); // зкорочує число з плаваючою крапкою залишає 3 цифри після крапки
+// // salary = Number(salary.toFixed(3)); //приводить до типу Number
+// console.log(Number(salary.toFixed(3)));
+ 
+console.log(Number(true));// тру приводиться до числа 1
+console.log(Number(false));//фолс приводиться до числа 0
+// 
