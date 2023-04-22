579c12e7096a4096ca7a45a3d178dbbcd08fa4b4
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-22 17:04:04 +0300

    масиви

--- js/task2.js
+++ js/task2.js
@@ -36,7 +36,37 @@
 // const deletedScores = scores.splice(0, 4);
 // console.log('scores:', scores);
 // console.log('deletedScores:', deletedScores);
-const colors = ['red', 'green', 'blue'];
-console.log(colors);
-colors.splice(1, 0, "purple");
-console.log(colors);
+// const colors = ['red', 'green', 'blue'];
+// console.log(colors);
+// colors.splice(1, 0, "purple");
+// console.log(colors);
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// clients.push("Vohra");
+// clients[3] = "Пушь";
+// console.log(clients);
+// clients.pop();
+// console.log(clients);
+
+//  let a = 10;
+//  let b = a;
+//  console.log(a, b);
+//  console.log(a === b);
+//  a = 30;
+//  console.log(a, b);
+//  console.log(a === b);
+
+//  const A = [1, 2, 3,];
+//  const B = A;
+//  A[0] = 10;
+//  console.log(A, B);
+//  console.log(A === B);
+
+// console.log(5 === 5);
+// console.log([5] === [5]);
+
+
+const friends = ['Mango', 'Kiws', 'Poly', 'Ajax'];
+for (let i = 0; i < friends.length; i += 1) {
+    console.log(i);
+}
