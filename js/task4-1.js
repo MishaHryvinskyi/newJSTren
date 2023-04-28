cc80cc4e2cdc6a577c79191281170f482fd5218f
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-28 14:23:39 +0300

    4-1

--- js/task4-1.js
+++ js/task4-1.js
@@ -1,9 +1,108 @@
-const numbers = [5, 10, 15, 20, 25];
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log("Масив numbers", numbers); 
+// dirtyMultiply(numbers, 2);
+
+// console.log("Масив numbers мутувався", numbers); 
+// // ---------------------------------------------
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers1, 2);
 
 
+// console.log("Масив numbers1", numbers1);
 
-// Метод перебирання forEach
-numbers.forEach(function (number, index) {
-    number *= 2;
-  console.log(`Індекс ${index}, значення ${number}`);
-});
+// console.log("Масив doubledNumbers", doubledNumbers); 
+
+// const planets = ["Земля", "Марс", "Венера", "Юпітер"];
+
+// const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+// console.log(planetsInUpperCase); 
+
+// const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+// console.log(planetsInLowerCase); 
+
+
+// console.log(planets); 
+
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+//   { name: "Х'юстон", score: 64 },
+// ];
+
+// const names = students.map(student => student.name);
+// console.log(names);
+
+// const score = students.map(students => students.score);
+// console.log(score);
+
+// const students = [
+//   { name: "Манго", courses: ["математика", "фізика"] },
+//   { name: "Полі", courses: ["інформатика", "математика"] },
+//   { name: "Ківі", courses: ["фізика", "біологія"] },
+// ];
+
+// const course = students.flatMap(student => student.courses);
+// console.log(course)
+
+// const values = [51, -3, 27, 21, -68, 42, -37];
+
+// const positiveValues = values.filter(value => value >= 0);
+// console.log(positiveValues); 
+
+// const negativeValues = values.filter(value => value < 0);
+// console.log(negativeValues); 
+
+// const bigValues = values.filter(value => value > 1000);
+// console.log(bigValues); 
+
+// console.log(values);
+
+// const students = [
+//   { name: "Манго", courses: ["математика", "фізика"] },
+//   { name: "Полі", courses: ["інформатика", "математика"] },
+//   { name: "Ківі", courses: ["фізика", "біологія"] },
+// ];
+
+// const allCourses = students.flatMap(student => student.courses);
+// console.log(allCourses)
+// const uniqueCourses = allCourses.filter((course, index, array) => array.indexOf(course) === index);
+// console.log(uniqueCourses)
+
+// const LOW_SCORE = 50;
+// const HIGH_SCORE = 80;
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+//   { name: "Х'юстон", score: 64 },
+// ];
+// const best = students.filter(({ score }) => score >= HIGH_SCORE);
+// console.log(best);
+
+// const worst = students.filter(({ score }) => score < LOW_SCORE);
+// console.log(worst);
+// const average = students.filter(
+//   ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+// );
+// console.log(average);
+
+
