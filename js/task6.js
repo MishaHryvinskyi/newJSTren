94a909353f590d04b5a76840de0ea102c81c75fa
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-03 16:34:26 +0300

    task6

--- js/task6.js
+++ js/task6.js
@@ -46,7 +46,7 @@
 // list.insertAdjacentHTML("beforeend", markup);
 // list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
 
- const magicBtn = document.querySelector('.js-magic-btn');
+//  const magicBtn = document.querySelector('.js-magic-btn');
 
 // magicBtn.addEventListener('click', () => {
 //   const navEl = document.querySelector('.menu')
@@ -55,31 +55,190 @@
 // console.log(navItem);
 // })
 
-const imageEl = document.querySelector('.hero__image');
-imageEl.alt ='Це Катя!';
+// const imageEl = document.querySelector('.hero__image');
+// imageEl.alt ='Це Катя!';
 
-magicBtn.addEventListener('click', () => {
-  imageEl.src = 'https://scontent.flwo4-2.fna.fbcdn.net/v/t1.6435-9/118516277_714405615777388_7029838492332625200_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=84a396&_nc_ohc=gKGRtTD7kWgAX_eMAMM&_nc_ht=scontent.flwo4-2.fna&oh=00_AfCeLz3Btw9okP0tl8tEccunq1ktGTV_aB1lZD9UCapkAg&oe=6478F43A';
-  imageEl.alt = "Це файна качеча!"
-  heroTitle.textContent = "Це Пташка!";
-  console.log(heroTitle.textContent);
-  document.body.style.backgroundColor = "purple";
-  console.log(document.body.style.backgroundColor);
-  const inputEL = document.querySelector('.js-input');
-  console.log(inputEL.value);
-  const inputValue = document.createElement('h2');
-  inputValue.textContent =  inputEL.value;
-  inputEL.prepend('.js-input');
-});
+// magicBtn.addEventListener('click', () => {
+//   imageEl.src = 'https://scontent.flwo4-2.fna.fbcdn.net/v/t1.6435-9/118516277_714405615777388_7029838492332625200_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=84a396&_nc_ohc=gKGRtTD7kWgAX_eMAMM&_nc_ht=scontent.flwo4-2.fna&oh=00_AfCeLz3Btw9okP0tl8tEccunq1ktGTV_aB1lZD9UCapkAg&oe=6478F43A';
+//   imageEl.alt = "Це файна качеча!"
+//   heroTitle.textContent = "Це Пташка!";
+//   const inputEL = document.querySelector('.js-input');
+//   console.log(inputEL.value);
+//   const inputValue = document.createElement('h2');
+//   inputValue.textContent =  inputEL.value;
+  
+//   // navEl.classList.toggle('kaaaaaaaaaaa');
+//   // navEl.classList.replace('menu', 'meeeeeee')
+// });
 
-const heroTitle = document.querySelector('.hero__title');
-heroTitle.textContent = "Це Катічка!";
+// const heroTitle = document.querySelector('.hero__title');
+// heroTitle.textContent = "Це Катічка!";
 
 
 // imageEl.removeAttribute('src');
  
-const actions = document.querySelectorAll('.js-actions button');
-// console.log(actions[0].dataset.action);
-// console.log(actions[1].dataset.action);
-// console.log(actions[2].dataset.action);
+// const actions = document.querySelectorAll('.js-actions button');
+// // console.log(actions[0].dataset.action);
+// // console.log(actions[1].dataset.action);
+// // console.log(actions[2].dataset.action);
+// const navEl = document.querySelector('.menu');
+// // navEl.classList.add('super-puper', "me");
+// // navEl.classList.remove('super-puper');
+// // navEl.classList.remove('me');
+
+// const currentPageUrl = '/portfolio';
+// const linkEl = document.querySelector(`.site-nav__link[href="${currentPageUrl}"]`);
+// // linkEl.classList.add('site-nav__item--current');
+// linkEl.classList.add('site-nav__item--currents');
+// const menuEl = document.querySelector('.menu');
+// // const firstEl = menuEl.querySelector('.menu-item');
+// const firstEl = menuEl.firstElementChild;
+// const lATEl = menuEl.lastElementChild;
+
+
+// const titleEl = document.createElement('h1');
+// titleEl.classList.add('ptashka');
+// titleEl.textContent = 'Качечиииий =)';
+
+// document.body.appendChild(titleEl);
+// const imagesEl = document.createElement('img');
+// imagesEl.src="https://scontent.flwo4-1.fna.fbcdn.net/v/t39.30808-6/278243342_3893263187565715_5154316446856748625_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=9KAU007eqXwAX9cZItg&_nc_ht=scontent.flwo4-1.fna&oh=00_AfBn9gGIbpjmw5z4eNVG8wvuw6UOqNX5g4Cu9dlDkvG5LQ&oe=6457A26C";
+// imagesEl.alt ="Це гобелен";
+// imagesEl.width = "600";
+// const paragraphEl = document.createElement('p');
+// paragraphEl.textContent = "Файний гобелен";
+
+
+// document.body.appendChild(imagesEl);
+// document.body.appendChild(paragraphEl);
+
+// const pathEl = document.createElement('img');
+// pathEl.src = 'https://scontent.flwo4-2.fna.fbcdn.net/v/t31.18172-8/28164780_2505082656383782_157716473639173224_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=84a396&_nc_ohc=3PDd1mvYZgQAX-IhaEU&_nc_ht=scontent.flwo4-2.fna&oh=00_AfC5nmIXovooFi7dw3afWLNeDkUtZbWrewMw4A6FBaADXA&oe=64799A83';
+// pathEl.alt = 'Тут має бути гобелен';
+// pathEl.width = "500";
+// document.body.appendChild(pathEl);
+
+// const navItemEl = document.createElement('li');
+// navItemEl.classList.add('menu-item');
+
+// const navLinkEl = document.createElement('a');
+// navLinkEl.classList.add('site-nav__link');
+// navLinkEl.textContent = 'Особистий кабінут';
+// navLinkEl.href = "/profile";
+
+// navItemEl.appendChild(navLinkEl);
+// menuEl.prepend(navItemEl);
+
+
+// const menuLinks = document.querySelector('.nav-menu');
+
+// const itemList1 = document.createElement('li');
+// itemList1.classList.add('item1');
+// const itemList2 = document.createElement('li');
+// itemList2.classList.add('item1');
+// const itemList3 = document.createElement('li');
+// itemList3.classList.add('item1');
+// const itemList4 = document.createElement('li');
+// itemList4.classList.add('item1');
+
+// const refsSrc = {
+//   src1: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t1.6435-9/43666858_2705786529646726_5288207087682191360_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=oPEKXTD2wEEAX8z7Gur&_nc_ht=scontent.flwo4-2.fna&oh=00_AfCO3cN6F_6z4XGQDQt7RhBBvPRnd27fh1v7OVnngOC8gA&oe=6479B9FF',
+//   src2: 'https://scontent.flwo4-1.fna.fbcdn.net/v/t1.6435-9/45011805_682050222195278_4901538174230069248_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=rRomDnvlzgUAX-mg-Kr&_nc_ht=scontent.flwo4-1.fna&oh=00_AfCPkc8xbFXZYJPIzNONoQW5rpLMl2MMJNtLB7haplQccg&oe=6479B9F2',
+//   src3: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t31.18172-8/15391399_2198325463726171_5944120123279349034_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NbbBwnPD7ZMAX8JXg3k&_nc_ht=scontent.flwo4-2.fna&oh=00_AfAgC5r6ajYtSB8ny3Y_wxv_kj1v_WblIQvYsuskt7HRfw&oe=6479B06C',
+//   src4: 'https://scontent.flwo4-1.fna.fbcdn.net/v/t1.6435-9/209569335_3643105062581530_586378542254498003_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cS9ZPhz3B20AX_skefR&_nc_ht=scontent.flwo4-1.fna&oh=00_AfA70U31FUlGQOyaaFcwPbIYoGgfhb9HHEUzWlQVE4W5hg&oe=6479BFE6',
+//   width : 400,
+// }
+
+// const itemPhotos1 = document.createElement('img');
+// itemPhotos1.src = `${refsSrc.src1}`;
+// itemPhotos1.alt = "Ми в Єгипті";
+// itemPhotos1.width = `${refsSrc.width}`;
+
+
+// itemList1.appendChild(itemPhotos1);
+
+// const itemPhotos2 = document.createElement('img');
+// itemPhotos2.src = `${refsSrc.src2}`;
+// itemPhotos2.alt = 'Ми у вишиванках';
+// itemPhotos2.width = `${refsSrc.width}`;
+
+// itemList2.appendChild(itemPhotos2);
+
+// const itemPhotos3 = document.createElement('img');
+// itemPhotos3.src = `${refsSrc.src3}`;
+// itemPhotos3.alt = "Ми засніжені";
+// itemPhotos3.width = `${refsSrc.width}`;
+
+// itemList3.appendChild(itemPhotos3);
+
+// const itemPhotos4 = document.createElement('img');
+// itemPhotos4.src = `${refsSrc.src4}`;
+// itemPhotos4.alt = "Ми в Чорногорії";
+// itemPhotos4.width = `${refsSrc.width}`;
+
+// itemList4.appendChild(itemPhotos4);
+
+// menuLinks.append(itemList4, itemList3, itemList2, itemList1);
+
+
+// const colorPickerOptions = [
+//     { label: 'red', color: '#F44336' },
+//     { label: 'green', color: '#4CAF50' },
+//     { label: 'blue', color: '#2196F3' },
+//     { label: 'grey', color: '#607D8B' },
+//     { label: 'pink', color: '#E91E63' },
+//     { label: 'indigo', color: '#3F51B5' },
+// ];
+
+// const colorPickerContainer = document.querySelector('.js-color-picker');
+
+// const makeColorPickerOptions = options => {
+//   return options.map(({ label, color }) => {
+//     const buttonEl = document.createElement('button');
+//      buttonEl.type = 'button';
+//      buttonEl.textContent = label;
+//      buttonEl.style.backgroundColor = color;
+//      return buttonEl;
+    
+//   })
+// };
+// const elements = makeColorPickerOptions(colorPickerOptions);
+// colorPickerContainer.append(...elements);
+
+
+
+
+
+// const product = {
+//   name: 'Сервоприводи',
+//   description: 'Lorem, ipsum dolor sit amet consectetur',
+//   price: 2000,
+//   availabel: true,
+//   onSale: true,
+// };
+
+// const productEl = document.createElement('article');
+// productEl.classList.add('product');
+
+// const nameEl = document.createElement('h2');
+// nameEl.textContent = product.name;
+// nameEl.classList.add('product__name');
+
+// const descrEl = document.createElement('p');
+// descrEl.textContent = product.description;
+// descrEl.classList.add('product__descr');
+
+// const priceEl = document.createElement('p');
+// priceEl.textContent = `Ціна: ${product.price} грн`;
+// priceEl.classList.add('product__price');
+
+// const onSaleEl = document.createElement('p');
+// onSaleEl.textContent = product.onSale;
+// onSaleEl.classList.add('product__onSale');
+
+// productEl.append(nameEl, descrEl, priceEl, onSaleEl);
+
+// console.log(productEl);
+
+// document.body.append(productEl);
 
