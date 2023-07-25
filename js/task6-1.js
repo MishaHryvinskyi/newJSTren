8f0cda50d083efb2b85a11345eab01dc37c07409
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-07-25 18:55:19 +0300

    6-2

--- js/task6-1.js
+++ js/task6-1.js
@@ -132,55 +132,55 @@
 
 // rootEl.append(titleEl, imgEl);
 
-const colorPickerOptions = [
-    { label: 'red', color: '#F44336' },
-    { label: 'green', color: '#4CAF50' },
-    { label: 'blue', color: '#2196F3' },
-    { label: 'grey', color: '#607D8B' },
-    { label: 'pink', color: '#E91E63' },
-    { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//     { label: 'red', color: '#F44336' },
+//     { label: 'green', color: '#4CAF50' },
+//     { label: 'blue', color: '#2196F3' },
+//     { label: 'grey', color: '#607D8B' },
+//     { label: 'pink', color: '#E91E63' },
+//     { label: 'indigo', color: '#3F51B5' },
+// ];
 
-const buttonEl = document.createElement('button');
-buttonEl.type = "button";
-buttonEl.textContent = colorPickerOptions[1].label;
-buttonEl.style.backgroundColor = colorPickerOptions[1].color;
+// const buttonEl = document.createElement('button');
+// buttonEl.type = "button";
+// buttonEl.textContent = colorPickerOptions[1].label;
+// buttonEl.style.backgroundColor = colorPickerOptions[1].color;
 
-console.log(buttonEl);
+// console.log(buttonEl);
 
-const colorPickerEl = document.querySelector('.color-picker');
+// const colorPickerEl = document.querySelector('.color-picker');
 
 // colorPickerEl.appendChild(buttonEl);
 
-const makeColorPicker = colorPickerOptions
-.map(colorPick => 
-`<button style=" background-color: ${colorPick.color}" type="button" >${colorPick.label}</button>`)
-.join('');
+// const makeColorPicker = colorPickerOptions
+// .map(colorPick => 
+// `<button style=" background-color: ${colorPick.color}" type="button" >${colorPick.label}</button>`)
+// .join('');
 
-colorPickerEl.insertAdjacentHTML("beforeend", makeColorPicker);
+// colorPickerEl.insertAdjacentHTML("beforeend", makeColorPicker);
 
-const picturesData = [
-    {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t39.30808-6/321794226_1561818460897261_1861313448952813905_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=TdgVtjd6rB8AX8aOnJZ&_nc_ht=scontent.flwo4-2.fna&oh=00_AfDsOUPv8FYUSLT4eYpteHKMwP9mBQ6g7jxccG9KUFnn_g&oe=64C29739', alt: 'Різдвяник'},
-    {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t31.18172-8/28164780_2505082656383782_157716473639173224_o.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=84a396&_nc_ohc=NKBmOIkGVH8AX9WTI8D&_nc_ht=scontent.flwo4-2.fna&oh=00_AfBnl_lkVXXDqacwe6Yty08cS551g9zMk5uZSpDCbiv8VQ&oe=64E50CC3', alt: 'Шлях'},
-    {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t39.30808-6/313203111_4077233002502065_8750628107843701626_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=aNQZgGX3qfkAX8nGxkQ&_nc_ht=scontent.flwo4-2.fna&oh=00_AfBVmCzUn3LNvgFfntxAUWUjX0qYjfTsLnOhq0JWZZ4L1w&oe=64C37867', alt: 'три'},
-    {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t1.6435-9/31682413_2554601674765213_5916495472339451904_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_ohc=-74H-4c4hrsAX8QuVWO&_nc_ht=scontent.flwo4-2.fna&oh=00_AfDr04fJ62pa5blRFetGw7Glim9dMJnTziPOa1WQ22Y5ig&oe=64E52023', alt: 'лч'},
-];
+// const picturesData = [
+//     {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t39.30808-6/321794226_1561818460897261_1861313448952813905_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=TdgVtjd6rB8AX8aOnJZ&_nc_ht=scontent.flwo4-2.fna&oh=00_AfDsOUPv8FYUSLT4eYpteHKMwP9mBQ6g7jxccG9KUFnn_g&oe=64C29739', alt: 'Різдвяник'},
+//     {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t31.18172-8/28164780_2505082656383782_157716473639173224_o.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=84a396&_nc_ohc=NKBmOIkGVH8AX9WTI8D&_nc_ht=scontent.flwo4-2.fna&oh=00_AfBnl_lkVXXDqacwe6Yty08cS551g9zMk5uZSpDCbiv8VQ&oe=64E50CC3', alt: 'Шлях'},
+//     {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t39.30808-6/313203111_4077233002502065_8750628107843701626_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=aNQZgGX3qfkAX8nGxkQ&_nc_ht=scontent.flwo4-2.fna&oh=00_AfBVmCzUn3LNvgFfntxAUWUjX0qYjfTsLnOhq0JWZZ4L1w&oe=64C37867', alt: 'три'},
+//     {src: 'https://scontent.flwo4-2.fna.fbcdn.net/v/t1.6435-9/31682413_2554601674765213_5916495472339451904_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_ohc=-74H-4c4hrsAX8QuVWO&_nc_ht=scontent.flwo4-2.fna&oh=00_AfDr04fJ62pa5blRFetGw7Glim9dMJnTziPOa1WQ22Y5ig&oe=64E52023', alt: 'лч'},
+// ];
 
-const makePhoto = picturesData
-    .map(pik =>
-    `<li><img src="${pik.src}" alt="${pik.alt}" width="300"></li>`)
-    .join('');
+// const makePhoto = picturesData
+//     .map(pik =>
+//     `<li><img src="${pik.src}" alt="${pik.alt}" width="300"></li>`)
+//     .join('');
 
-    colorPickerEl.insertAdjacentHTML("beforeend", makePhoto);
+//     colorPickerEl.insertAdjacentHTML("beforeend", makePhoto);
 
-const product = {
-    name: "Сервопроводи",
-    description: 
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex assumenda minus commodi saepe ea mollitia consequatur deserunt rerum provident itaque, voluptas velit nihil, nam excepturi id hic magni dolorum iste.",
-    price: 2000,
-    available: true,
-    onSale: true,
-};
+// const product = {
+//     name: "Сервопроводи",
+//     description: 
+//     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex assumenda minus commodi saepe ea mollitia consequatur deserunt rerum provident itaque, voluptas velit nihil, nam excepturi id hic magni dolorum iste.",
+//     price: 2000,
+//     available: true,
+//     onSale: true,
+// };
 
 
 // const marcupProduct = `
@@ -193,27 +193,154 @@ const product = {
 
 // colorPickerEl.insertAdjacentHTML("beforeend", marcupProduct);
 
-const { name, description, price } = product;
+// const { name, description, price } = product;
 
 
 
-const productArticleEl = document.createElement('article');
-productArticleEl.classList = "product";
+// const productArticleEl = document.createElement('article');
+// productArticleEl.classList = "product";
 
-const productTitleEl = document.createElement('h2');
-productTitleEl.textContent = name;
-productTitleEl.classList = "product__name";
+// const productTitleEl = document.createElement('h2');
+// productTitleEl.textContent = name;
+// productTitleEl.classList = "product__name";
 
-const productParagraphEl = document.createElement('p');
-productParagraphEl.textContent = description;
-productParagraphEl.classList = "product__descr";
+// const productParagraphEl = document.createElement('p');
+// productParagraphEl.textContent = description;
+// productParagraphEl.classList = "product__descr";
 
-const productPriceEl = document.createElement('p');
-productPriceEl.textContent = `Ціна: ${price} грн`;
-productPriceEl.classList = "product__price";
+// const productPriceEl = document.createElement('p');
+// productPriceEl.textContent = `Ціна: ${price} грн`;
+// productPriceEl.classList = "product__price";
 
-productArticleEl.append(productTitleEl, productParagraphEl, productPriceEl);
-console.log(productArticleEl);
+// productArticleEl.append(productTitleEl, productParagraphEl, productPriceEl);
+// console.log(productArticleEl);
+// colorPickerEl.append(productArticleEl);
 
-colorPickerEl.append(productArticleEl);
 
+
+//---1. Створити список технологій що використовуються на курсі Fullstack за допомогою js
+
+// const container = document.getElementById('container'); 
+// //шукаємо контейнер в нашому випадку <div> за його id
+
+// const technologies = [ 'HTML', 'CSS', 'JavaScript', 'React', 'Node' ];
+// масив технологій який потрібно розприділити по <li>шках
+
+// const listEl = document.createElement('ul');//Створюємо елемент <ul>
+// listEl.classList = 'list';//додаємо йому клас
+// // container.append(listEl);//і вносимо його в контейнер
+
+// * методом [map] моя спроба =================================
+// const markupListItems = technologies // оголошуємо змінну розмітки
+// .map(technology => // методом map розбиваємо наш масив на елементи
+// `<li class="list-item">${technology}</li>`)// а саме створюємо лішки з класами за допомогою шаблонного рядка і в кожну лішку додаємо по одному елементу масиву
+// .join('');// join розділяє рядок який ми отримали
+// listEl.innerHTML = markupListItems;// вставляємо розмітку з лішок у ul
+
+//* методом [for of]=============================
+// for (const technology of technologies) {
+
+//     const itemEl = document.createElement('li');
+//     // itemEl.textContent = technology;
+//     const text = document.createTextNode(technology); // createTextNode - створює не вузол елементу а вузол тексту
+//     itemEl.appendChild(text); //вставляємо текстовий вузол в лішки
+//     itemEl.classList = 'list-item';
+//     listEl.appendChild(itemEl);
+// };
+
+// метод [.map()] ==============================
+
+//  Варіант з шаблонним рядком дя всієї розмітки
+// const marcup = 
+// `<ul class="list">
+//     ${
+//         technologies.map(technology => 
+//             `<li class="list-item">
+//                 ${technology}
+//             </li>`)
+//             .join('')
+//     }
+// </ul>`;
+
+// document.body.insertAdjacentHTML("afterbegin", marcup);
+
+//  Метод [reduce] ====================
+
+// const markup = 
+// `<ul class="list">${technologies.reduce(
+//     (acc, technology) => acc += `<li class="list-item">${technology}</li>`,
+//     ""
+// )}</ul>`;
+
+// console.log(markup);
+// document.body.insertAdjacentHTML("afterbegin", markup);
+
+//---2. Створити кілька кнопок на основі масива з обєктами використовуючи createElement
+
+// const colors = [
+//     { 
+//         label: "red",
+//         color: '#FF0000',
+//     },
+//     { 
+//         label: "green",
+//         color: '#00FF00',
+//     },
+//     { 
+//         label: "blue",
+//         color: '#0000FF',
+//     },
+//     { 
+//         label: "yellow",
+//         color: '#FFFF00',
+//     },
+// ];
+
+// const containerEl = document.getElementById('container');
+
+// ==== метод [for... of] ==========
+
+// for (const color of colors) {
+//     const btnEl = document.createElement('button');
+//     const textBtn = document.createTextNode(color.label);
+//     btnEl.appendChild(textBtn);
+//     btnEl.style.backgroundColor = color.color;
+//     btnEl.type = "button";
+//     console.log(btnEl);
+//     containerEl.appendChild(btnEl);
+// };
+
+// ==== метод [map] =========
+
+// мій варіант
+// const markupBtn = colors.map(({ label, color }) =>
+//     `<button type="button" style="background-color: ${color}">
+//         ${label}
+//     </button>`
+//     ).join('');
+//     console.log(markupBtn);
+//     document.body.insertAdjacentHTML('afterbegin', markupBtn);
+
+// ===========================
+
+// тут може бути або map або forEach
+// const fragment = document.createDocumentFragment(); //cтворення фрагменту який замість чотири рази вставляти кнопку робить це за раз!!!
+
+// colors.forEach(({ color, label }) => {
+//     const buttonEl = document.createElement('button');
+//     buttonEl.textContent = label;
+//     buttonEl.style.fontWeight = 800;
+//     buttonEl.type = 'button';
+//     buttonEl.style.backgroundColor = color;
+
+//     fragment.appendChild(buttonEl);
+//     // document.body.appendChild(buttonEl);
+// });
+// document.body.appendChild(fragment);
+
+// const itemsEl = document.querySelectorAll('.item');
+
+// itemsEl.forEach(el => {
+//     console.log(el.firstElementChild.textContent);
+//     console.log(el.lastElementChild.children.length);
+// });
--- js/task6-2.js
+++ js/task6-2.js
@@ -1,25 +1,69 @@
-const Btn = document.querySelector('.Btns');
-console.log(Btn);
-const handleClick = event => {
-    console.log("event: ", event);
-    console.log("event type: ", event.type);
-    console.log("target: ", event.target);
-}
+// const Btn = document.querySelector('.Btns');
+// console.log(Btn);
+// const handleClick = event => {
+//     console.log("event: ", event);
+//     console.log("event type: ", event.type);
+//     console.log("target: ", event.target);
+// }
 
-Btn.addEventListener("click", handleClick);
+// Btn.addEventListener("click", handleClick);
 
-document.addEventListener("keydown", e => {
-    console.log("keydown:", e);
-    console.log("key:", e.key);
-    console.log("code:", e.code);
+// document.addEventListener("keydown", e => {
+//     console.log("keydown:", e);
+//     console.log("key:", e.key);
+//     console.log("code:", e.code);
+// })
+
+// document.addEventListener("keydown", event => {
+//     event.preventDefault();
+  
+//     if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
+//       console.log("«Ctrl + s» or «Command + s» combo");
+//     }
+//   });
+
+// const btn = document.querySelector('.btn');
+
+
+// btn.addEventListener("click", () => {
+//   console.log('тицьнули в кніпочку!');
+// });
+
+// const btn = document.querySelector('.btn');
+
+// const handleClick = e => {
+//   e.preventDefault();
+//   const title = document.createElement('img');
+//   title.width = 200;
+//   title.src = "https://report.if.ua/wp-content/uploads/2021/04/zdyvuvannja.jpeg";
+//   document.body.appendChild(title);
+//   // console.log(e.type);
+//   // console.log(e.currentTarget);
+// };
+
+// btn.addEventListener("click", handleClick);
+// btn.addEventListener("keydown", e => {
+//   console.log(e.key);
+//   console.log(e.code);
+// });
+
+const targetBtn = document.querySelector('.js-target-btn');
+const addListenerBtn = document.querySelector('.js-add-listener');
+const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+
+addListenerBtn.addEventListener('click', () => {
+  console.log('вішаю слухача подій на цільову кнопку');
+
+  targetBtn.addEventListener('click', onTargetBtnClick)
+});
+
+removeListenerBtn.addEventListener('click', () => {
+  console.log('знімаю слухача подій');
+
+  targetBtn.removeEventListener('click', onTargetBtnClick)
 })
-
-document.addEventListener("keydown", event => {
-    event.preventDefault();
   
-    if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
-      console.log("«Ctrl + s» or «Command + s» combo");
-    }
-  });
-
-  
+function onTargetBtnClick() {
+  console.log("Клік по цільовій кнопці");
+};
