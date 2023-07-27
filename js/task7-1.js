66d288c6a0ed44bcdae7dd9f45b5439288b95b1b
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-07-28 00:24:14 +0300

    d

--- js/task7-1.js
+++ js/task7-1.js
@@ -25,12 +25,12 @@
 // }
 // ========================Фільтрація==============================
 
-const tagsContainer = document.querySelector('.js-tags');
+// const tagsContainer = document.querySelector('.js-tags');
 // let selectedTag = null;
-let selectedTag = new Set();
+// let selectedTag = new Set();
 
 // tagsContainer.addEventListener('click', onTagBtnClick);
-tagsContainer.addEventListener('click', onTagsContainerClick);
+// tagsContainer.addEventListener('click', onTagsContainerClick);
 
 // function onTagBtnClick(event) {
 //     if(event.target.nodeName !== 'BUTTON') {
@@ -50,12 +50,65 @@ tagsContainer.addEventListener('click', onTagsContainerClick);
 //     console.log(selectedTag);
 // }
 
-function onTagsContainerClick(evt) {
-    if(evt.target.nodeName !== 'BUTTON') {
-        return;
-    }
+// function onTagsContainerClick(evt) {
+//     if(evt.target.nodeName !== 'BUTTON') {
+//         return;
+//     }
 
-    evt.target.classList.toggle('tags-button--active');
-    selectedTag.add(evt.target.dataset.action)
-    console.log(selectedTag);
-}
+//     evt.target.classList.toggle('tags-button--active');
+//     selectedTag.add(evt.target.dataset.action)
+//     console.log(selectedTag);
+// }
+
+// ==========================ColorPicker===========================
+
+const colors = [
+    { hex: '#f44336', rgb: '244,67,54' },
+    { hex: '#e91e63', rgb: '233,30,99' },
+    { hex: '#9c27b0', rgb: '156,39,176' },
+    { hex: '#673ab7', rgb: '103,58,183' },
+    { hex: '#3f51b5', rgb: '63,81,181' },
+    { hex: '#2196f3', rgb: '33,150,243' },
+    { hex: '#00bcd4', rgb: '0,188,212' },
+    { hex: '#009688', rgb: '0,150,136' },
+    { hex: '#4caf50', rgb: '76,175,80' },
+    { hex: '#ffeb3b', rgb: '255,235,59' },
+    { hex: '#ff9800', rgb: '255,152,0' },
+    { hex: '#795548', rgb: '121,85,72' },
+    { hex: '#607d8b', rgb: '96,125,139' },
+];
+
+const colorPicker = document.querySelector('.color-picker');
+
+// const colorPickerMarcup = colors
+//     .map(color => 
+//     `<li class="color-picker__item" style="background-color: ${color.hex}">
+//         <p class="color-picker__paragraph">${color.rgb}</p>
+//     </li>`)
+//     .join('');
+
+// colorPicker.insertAdjacentHTML("beforeend", colorPickerMarcup);
+
+console.log(createColorCardsMarcup(colors))
+
+function createColorCardsMarcup(colors) {
+    return colors.map(({ hex, rgb }) => { 
+        return`
+            <div class="color-card">
+                <div 
+                    class="color-swatch"
+                    data-hex="${hex}"
+                    data-rgb="${rgb}"
+                    style="background-color: ${hex}"
+                >
+                    <div class="color-meta">
+                        <p>HEX: ${hex}</p>
+                        <p>RGB: ${rgb}</p>
+                    </div>
+                </div>
+            </div>`; 
+        }).join('');
+};
+
+colorPicker.insertAdjacentHTML("beforeend", createColorCardsMarcup(colors))
+
