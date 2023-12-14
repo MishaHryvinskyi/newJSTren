5a54cffb3be1b70d06055346798cc9a8f1bdc8cf
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-12-14 16:11:54 +0200

    dw

--- js/task3-2.js
+++ js/task3-2.js
@@ -349,3 +349,14 @@ console.log("==========================");
 //     email: 'a.cross@hotmail.com',
 //     friendCount: 20,
 // }));
+
+const highTemperatures = {
+    yesterday: 28,
+    today: 26,
+    tomorrow: 33,
+  };
+  const { yesterday, today, tomorrow } = highTemperatures;
+  
+  
+  const meanTemperature = (yesterday + today + tomorrow) / 3;
+  console.log(meanTemperature)
